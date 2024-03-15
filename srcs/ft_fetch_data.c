b6530023319521a15281d8afacd7cdad35702feb
Mehdi DERKAOUI <mderkaou@bess-f2r5s9.clusters.42paris.fr>
2024-03-15 19:00:05 +0100

    norm

--- srcs/ft_fetch_data.c
+++ srcs/ft_fetch_data.c
@@ -5,24 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mderkaou <mderkaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2024/03/15 17:09:31 by mderkaou         ###   ########.fr       */
+/*   Created: 2024/03/15 18:20:27 by mderkaou          #+#    #+#             */
+/*   Updated: 2024/03/15 18:56:49 by mderkaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-/* ------------------------------- map_cpy() ---------------------------------- */
+/* ---------------- map_cpy() ------------------- */
 /*
 **		copie la map depuis parse.
 */
 
-int map_cpy(t_parse *parse, t_mlx *data)
+int	map_cpy(t_parse *parse, t_mlx *data)
 {
-	int i;
+	int	i;
 
 	i = 0;
-   	data->lvl = malloc(sizeof(t_map));
+	data->lvl = malloc(sizeof(t_map));
 	if (data->lvl == NULL)
 		return (printf(RE "Error: Couldn't allocate map.\n" RESET), 1);
 	data->lvl->lenX = parse->max_len;
@@ -39,7 +39,7 @@ int map_cpy(t_parse *parse, t_mlx *data)
 	return (0);
 }
 
-/* ------------------------------- fetch_map_data() ---------------------------------- */
+/* --------------- fetch_map_data() ---------------------- */
 /*
 **		récupère les datas de la map depuis parse.
 */
@@ -52,29 +52,31 @@ int	fetch_map_data(t_mlx *data, t_parse *parse)
 	return (0);
 }
 
-/* ------------------------------- rgb_to_int() ---------------------------------- */
+/* --------------------- rgb_to_int() ----------------------- */
 /*
 **		convertit une valeur rgb en int grâce à des décalages binaires.
 */
 
-int		rgb_to_int(int r, int g, int b)
+int	rgb_to_int(int r, int g, int b)
 {
 	return ((r << 16) | (g << 8) | b);
 }
 
-/* ------------------------------- init_textures() ---------------------------------- */
+/* --------------------- init_textures() ------------------- */
 /*
 **		ouvre les images xpm to image mlx.
 */
 
 void	fetch_xpm_image(void **mlx, t_img *asset, char **textures, int id)
 {
-	asset->mlx_img = mlx_xpm_file_to_image(*mlx, textures[id], &asset->width, &asset->height);
-	asset->addr = (int *)mlx_get_data_addr(asset->mlx_img, &asset->bpp, &asset->line_len, &asset->endian);
+	asset->mlx_img = mlx_xpm_file_to_image(*mlx, textures[id], &asset->width,
+			&asset->height);
+	asset->addr = (int *)mlx_get_data_addr(asset->mlx_img, &asset->bpp,
+			&asset->line_len, &asset->endian);
 	return ;
 }
 
-/* ------------------------------- init_textures() ---------------------------------- */
+/* ----------------------- init_textures() ------------------ */
 /*
 **		récupère les textures et les stockes dans data.
 */
@@ -85,10 +87,14 @@ void	init_textures(t_parse *parse, t_mlx *data)
 	if (data->assets == NULL)
 		return (printf("Error\nMalloc\n"), ft_free_map(parse), exit(0));
 	assets_init(data->assets);
-	fetch_xpm_image(&data->mlx, data->assets->nwall, parse->textures, parse->n_id);
-	fetch_xpm_image(&data->mlx, data->assets->swall, parse->textures, parse->s_id);
-	fetch_xpm_image(&data->mlx, data->assets->wwall, parse->textures, parse->w_id);
-	fetch_xpm_image(&data->mlx, data->assets->ewall, parse->textures, parse->e_id);
+	fetch_xpm_image(&data->mlx, data->assets->nwall, parse->textures,
+		parse->n_id);
+	fetch_xpm_image(&data->mlx, data->assets->swall, parse->textures,
+		parse->s_id);
+	fetch_xpm_image(&data->mlx, data->assets->wwall, parse->textures,
+		parse->w_id);
+	fetch_xpm_image(&data->mlx, data->assets->ewall, parse->textures,
+		parse->e_id);
 	data->assets->floor = rgb_to_int(parse->rgb[0], parse->rgb[1],
 			parse->rgb[2]);
 	data->assets->ceiling = rgb_to_int(parse->rgb[3], parse->rgb[4],
--- srcs/ft_init_game.c
+++ srcs/ft_init_game.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_init_game.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flplace <flplace@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mderkaou <mderkaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 15:19:14 by mderkaou          #+#    #+#             */
-/*   Updated: 2024/03/15 17:53:58 by flplace          ###   ########.fr       */
+/*   Updated: 2024/03/15 18:59:05 by mderkaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-/* ------------------------------- move_update() -------------------------------- */
+/* -------------------- move_update() ----------------- */
 /*
 **		applique les mouvements.
 */
@@ -32,46 +32,53 @@ void	move_update(t_mlx *data)
 	return ;
 }
 
-
-/* --------------------------- loop_process() ------------------------------- */
+/* ---------------- loop_process() ---------------------- */
 /*
 **		fonction appelée par mlx_loop_hook(),
-**		gère le comportement par défaut s'il n'y a pas d'action particulière.
-**		en l'état, il cherche la position du joueur et render la minimap en conséquence.
+**		gère le comportement par défaut
+		s'il n'y a pas d'action particulière.
+**		en l'état, il cherche la position du joueur
+		et render la minimap en conséquence.
 **
-**		étant donné que la structure de la map ne bouge pas, à voir si on ne pourrait pas
-**		juste mettre l'affichage du joueur là dedans et l'affichage de la map en dehors de
+**		étant donné que la structure de la map ne bouge pas,
+			à voir si on ne pourrait pas
+**		juste mettre l'affichage du joueur là dedans
+			et l'affichage de la map en dehors de
 **		la boucle pour nous sauver de la perf.
 **
-**		de la façon dont j'ai géré l'affichage, on écrit des pixels directement dans une image
-**		et on l'envoie ensuite à chaque frame à travers mlx_put_image_to_window().
+**		de la façon dont j'ai géré l'affichage,
+		on écrit des pixels directement dans une image
+**		et on l'envoie ensuite à chaque frame
+			à travers mlx_put_image_to_window().
 */
 
 int	loop_process(t_mlx *data)
 {
 	move_update(data);
 	main_process(data);
-	mlx_put_image_to_window(data->mlx, data->win, data->raycasting.mlx_img, 0, 0);
+	mlx_put_image_to_window(data->mlx, data->win, data->raycasting.mlx_img, 0,
+		0);
 	return (0);
 }
-/* -----------------------------escape() ------------------------------- */
+/* -----------------------escape() -------------------- */
 /*
 **		wrapper pour le clear_data hook.
 */
 
-
 int	escape(t_mlx *data)
 {
 	clear_all(data);
-	return 1;
+	return (1);
 }
-/* ----------------------------- game launcher() ------------------------------- */
+/* -------------------- game launcher() ------------------------ */
 /*
-**		initialise tous les éléments de data utiles pour faire tourner la MLX, à savoir:
+**	initialise tous les éléments de data utiles pour faire tourner la MLX,
+		à savoir:
 **		- data->mlx : le pointeur mlx *
 **		- data->win : le pointeur vers la window ouverte avec la mlx
-**		- data->raycasting.mlx_img : le pointeur vers une nouvelle image, actuellement utilisée pour la minimap
-**								(peut être à décaler ailleurs si on en fait une autre pour la projection 3D)
+**		- data->raycasting.mlx_img : le pointeur vers une nouvelle image,
+		actuellement utilisée pour la minimap
+**		(peut être à décaler ailleurs si on en fait une autre pour la projection 3D)
 **		- data->raycasting.addr : l'adresse de cette nouvelle image
 **		gère les hooks avant de lancer la loop.
 */
@@ -82,8 +89,9 @@ int	game_launcher(t_mlx *data)
 	if (data->win == NULL)
 		return (1);
 	data->raycasting.mlx_img = mlx_new_image(data->mlx, WINWIDTH, WINHEIGHT);
-	data->raycasting.addr = (int *)mlx_get_data_addr(data->raycasting.mlx_img, &data->raycasting.bpp,
-			&data->raycasting.line_len, &data->raycasting.endian);
+	data->raycasting.addr = (int *)mlx_get_data_addr(data->raycasting.mlx_img,
+			&data->raycasting.bpp, &data->raycasting.line_len,
+			&data->raycasting.endian);
 	mlx_loop_hook(data->mlx, &loop_process, data);
 	mlx_hook(data->win, 17, 1L << 5, escape, data);
 	mlx_hook(data->win, KeyPress, KeyPressMask, &keypress_hook, data);
@@ -92,10 +100,12 @@ int	game_launcher(t_mlx *data)
 	return (0);
 }
 
-/* ------------------------------- data_builder() -------------------------------- */
+/* --------------- data_builder() ---------------------- */
 /*
-**		initialise la structure de données data et lance le game_launcher.
-**		voir si ça peut pas être décalé dans game_launcher directement après avoir viré init_testmap.
+**		initialise la structure de données data et
+			lance le game_launcher.
+**		voir si ça peut pas être décalé dans
+		game_launcher directement après avoir viré init_testmap.
 */
 
 int	orientation_finder(t_mlx *data, char c)
@@ -131,15 +141,17 @@ int	orientation_finder(t_mlx *data, char c)
 	return (0);
 }
 
-/* ------------------------------- data_builder() -------------------------------- */
+/* ------------------ data_builder() ------------------- */
 /*
-**		initialise la structure de données data et lance le game_launcher.
-**		voir si ça peut pas être décalé dans game_launcher directement après avoir viré init_testmap.
+**		initialise la structure de données data et
+			lance le game_launcher.
+**		voir si ça peut pas être décalé dans
+		game_launcher directement après avoir viré init_testmap.
 */
 
 int	data_builder(t_parse *parse, t_mlx *data)
 {
-	char orientation;
+	char	orientation;
 
 	data_init(data);
 	if (data == NULL)
@@ -152,4 +164,3 @@ int	data_builder(t_parse *parse, t_mlx *data)
 	data->pos->y = data->square->y + 0.5;
 	return (0);
 }
-
